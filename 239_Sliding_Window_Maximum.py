4ef935d3f7bd80ff30d598ad77141a3979b52f60
Abhishek <83511097+Abhishek786-dev@users.noreply.github.com>
2026-03-26 20:36:10 +0545

    Add sliding window maximum solution in Python
    Implement sliding window maximum algorithm using deque.

---
+++ 239_Sliding_Window_Maximum.py
@@ -0,0 +1,47 @@
+"""
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+"""
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        dq = deque()
+        res = []
+        for i in range(len(nums)):
+            while( dq and nums[dq[-1]] <= nums[i]):
+                dq.pop()
+            dq.append(i)
+            if dq:
+                if dq[0] <= i-k:
+                    dq.popleft()
+            
+            if i >= k-1:
+                res.append(nums[dq[0]])
+        return res
+            
+    
+
+obj = Solution()
+resp = obj.maxSlidingWindow([1,3,-1,-3,5,3,1,7], 3)
+print(resp)
